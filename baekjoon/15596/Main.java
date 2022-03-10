0be423143e181e9141c0d2662166074b0a799c2b
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-03-10 20:20:53 +0900

    정수합

---
+++ baekjoon/15596/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sibal = 0;
+
+        for(int i=0; i<3; i++){
+          int asdf = 0;
+          int sibal = 12;
+        }
+        if(true){
+          int qwer = 0;
+        }
+
+        System.out.println(qwer);
+    }
+}
