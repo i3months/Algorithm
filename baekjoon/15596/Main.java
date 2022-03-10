d18e6ea3df22d5058b27b9e4ddc6f9693d38e5a3
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-03-10 20:22:16 +0900

    정수합

--- baekjoon/15596/Main.java
+++ baekjoon/15596/Main.java
@@ -1,19 +1,10 @@
-import java.util.*;
-import java.io.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int sibal = 0;
-
-        for(int i=0; i<3; i++){
-          int asdf = 0;
-          int sibal = 12;
-        }
-        if(true){
-          int qwer = 0;
-        }
-
-        System.out.println(qwer);
-    }
+public class Test {
+  long sum(int[] a) {
+      long ans = 0;
+      for(int i=0; i<a.length; i++){
+          ans = ans + a[i];
+      }
+      
+      return ans;
+  }
 }
