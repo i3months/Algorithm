23c8abc8a2a8722499477e7cff2d2ea03e3cc2cf
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-03-24 11:53:31 +0900

    파일옮기기

---
+++ baekjoon/11943/Main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+  public class Main {
+    public static void main(String[] args) throws IOException{ 
+
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      Scanner sc = new Scanner(System.in);
+
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
+      int d = sc.nextInt();
+
+      int temp1 = Math.min(a+d,b+c);
+      
+
+      System.out.println(temp1);
+     
+      
+    }
+  }
