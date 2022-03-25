e5b4af99a0aca1eaa77936d88d7244edbf55e227
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-03-25 10:29:04 +0900

    소수구하기

---
+++ baekjoon/1929/Main.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+  public class Main {
+
+    public static boolean[] prime;
+    public static void main(String[] args) throws IOException{ 
+
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      
+      StringTokenizer st = new StringTokenizer(br.readLine());
+
+      int N = Integer.parseInt(st.nextToken());
+      int M = Integer.parseInt(st.nextToken());
+
+      prime_check(M);
+
+      for(int i = N; i<=M; i++){
+        if(prime[i] == false){
+          System.out.println(i);
+        }
+      }      
+      
+
+    }
+    static void prime_check(int n){
+      prime = new boolean[n+1];
+      prime[0] = true;
+      prime[1] = true;
+
+     
+
+     for(int i= 2; i< Math.sqrt(n); i++){
+       if(prime[i] == true){
+         continue;
+       }
+
+       for(int j= i*i; j<prime.length; j = j+i){
+         prime[j] = true;
+       }
+
+       
+     }
+      
+    }
+
+  }
