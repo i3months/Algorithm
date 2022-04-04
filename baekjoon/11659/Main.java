27b6b7fbb0d3eb61dc8d2b16694dd57c151aa834
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-04-04 12:57:47 +0900

    구간 합 구하기 4

---
+++ baekjoon/11659/Main.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        int arr_sum[] = new int[N];
+
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        for(int i=0; i<N; i++){
+            if(i == 0){
+               arr_sum[i] = arr[i];
+            }else{
+                arr_sum[i] = arr_sum[i-1] + arr[i];
+            }
+        }
+
+
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            
+            if(start == 1){
+                sb.append(arr_sum[end-1]+ "\n");    
+            }else{
+                sb.append(arr_sum[end-1] - arr_sum[start-2] + "\n");
+            }
+            
+        }
+
+        System.out.println(sb);
+
+    }
+}
+
