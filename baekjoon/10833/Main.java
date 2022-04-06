2ac41678fb7d134eff84e87dbe186185a5bd3799
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-04-06 13:07:07 +0900

    사과

---
+++ baekjoon/10833/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sum = sum + b%a;
+            
+
+        }
+        System.out.println(sum);
+
+    }
+}
