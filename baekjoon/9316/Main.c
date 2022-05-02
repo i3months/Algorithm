ced65aba3ae655fbac2a3ddc8df03ae085fddf7c
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-05-02 09:40:28 +0900

    Hello Judge

---
+++ baekjoon/9316/Main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    int N;
+    scanf("%d", &N);
+
+    for(int i=1; i<N+1; i++){
+        printf("Hello World, Judge %d!\n", i);
+    }
+
+
+}
