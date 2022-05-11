45be46562b8ed445969798e19eb103b8a0566ed4
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-05-11 13:00:11 +0900

    캥거루 세마리

---
+++ baekjoon/2965/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+
+		int max = Math.max(b-a, c-b);
+		System.out.println(max-1);
+
+
+	}
+}
