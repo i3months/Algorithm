021d2694537589b76fd97613d05e57e73036006f
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-05-17 00:10:19 +0900

    수행시간 1

---
+++ baekjoon/24262/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	static int N;
+	static int[] ans = new int[500001];
+	static int cnt;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+
+		System.out.println(1);
+		System.out.println(0);
+		
+	
+	}
+}
