93b4985dd97763c1cc2cc8d1f7de5452736bb368
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-05-20 11:48:23 +0900

    리모컨. 완전탐색

---
+++ baekjoon/1107/Main.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {	
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+
+		boolean[] broken = new boolean[10];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		for(int i=0; i<M; i++){
+			broken[Integer.parseInt(st.nextToken())] = true;
+		}
+
+		int min = Math.abs(N - 100);
+
+		for(int i=0; i<1000000; i++){
+			String str = String.valueOf(i);
+			int len = str.length();
+
+			boolean brk = false;
+
+			for(int j=0; j<len ;j++){
+				if(broken[str.charAt(j) - '0']){
+					brk = true;
+					break;
+				}
+			}
+
+			if(!brk){
+				int temp = Math.abs(N - i) + len;
+				min = Math.min(min, temp);
+			}
+		}
+		System.out.println(min);
+		
+
+	}
+}
