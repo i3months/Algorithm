8a92ab3873dae5e21f40b1d650c788afd7f318bb
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-05-21 22:04:58 +0900

    N과 M 5

---
+++ baekjoon/15654/Main.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {	
+
+	static int N;
+	static int M;
+	
+	static int[] arr = new int[10];
+	static boolean[] used = new boolean[10001];
+	static ArrayList<Integer> list = new ArrayList<>();
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine());
+
+		while(st.hasMoreTokens()){
+			list.add(Integer.parseInt(st.nextToken()));
+		}
+
+		Collections.sort(list);
+		
+
+		solve(0);
+
+	}
+
+	static void solve(int k){
+
+		if(k == M){
+			for(int i=0; i<M; i++){
+				System.out.print(arr[i] + " ");
+			}
+			System.out.println();
+			return;			
+		}
+
+		for(int i=0; i<N; i++){
+			if(!used[list.get(i)]){
+				arr[k] = list.get(i);
+				used[list.get(i)] = true;
+				solve(k+1);
+				used[list.get(i)] = false;
+			}
+		}
+		
+	}
+
+}
