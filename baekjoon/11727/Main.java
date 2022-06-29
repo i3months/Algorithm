b43592761ea0c12d1ebbf5f723294997ef93a52f
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-06-29 13:43:29 +0900

    2xn타일링2

---
+++ baekjoon/11727/Main.java
@@ -0,0 +1,23 @@
+	import java.util.*;
+	import java.io.*;
+	import java.math.*;
+
+	public class Main {							
+		
+		public static void main(String[] args) throws IOException {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	    
+			
+			int[] dp = new int[1001];
+
+			dp[1] = 1;
+			dp[2] = 3;
+			dp[3] = 5;
+
+			for(int i=4; i<1001; i++){
+				dp[i] = (dp[i-1] + (dp[i-2]*2)) % 10007;
+			}
+
+			System.out.println(dp[Integer.parseInt(br.readLine())]);
+			
+		}
+	}
