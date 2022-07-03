6a1f9889ef079fa33f2622a4e230501a15399e8c
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-07-03 13:28:30 +0900

    Zadanie probne

---
+++ baekjoon/8545/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {				
+	public static void main(String[] args) throws IOException {			
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	    				
+		
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(br.readLine());
+
+		System.out.println(sb.reverse());
+
+
+	}
+}
