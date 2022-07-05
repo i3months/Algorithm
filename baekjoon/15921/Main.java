8311e2fbda1f04acc3ba05f1adae1f0ce6ccc212
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-07-05 13:53:32 +0900

    마린보이

---
+++ baekjoon/15921/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {				
+	public static void main(String[] args) throws IOException {			
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	    				
+
+		int N = Integer.parseInt(br.readLine());
+
+		if(N == 0) {
+			System.out.println("divide by zero");
+		}else {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+		
+			for(int i = 0; i < N; i++) {
+				int M = Integer.parseInt(st.nextToken());
+			}			
+			System.out.println("1.00");
+	
+
+	}}
+}
