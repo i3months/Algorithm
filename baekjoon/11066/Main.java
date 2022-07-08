939efcb1f816de82cb4f46ff2db74a8e3eb4013b
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-07-08 15:51:24 +0900

    파일 합치기

--- baekjoon/11066/Main.java
+++ baekjoon/11066/Main.java
@@ -1,58 +1,51 @@
 import java.util.*;
 import java.io.*;
-import java.math.*;
 
-public class Main {				
+public class Main{
+	public static void main(String[] args)throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		for(int start=0; start<T; start++){
+
+			int K = Integer.parseInt(br.readLine());
+
+			int[][] dp = new int[K+1][K+1];
+			int[][] sum = new int[K+1][K+1]; // prefix sum
 	
-		static int[] arr;
-		static int[][] dp;
-		static int[][] sum;
-		public static void main(String[] args) throws IOException {			
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	    
-			
-			int T = Integer.parseInt(br.readLine());				
+			int[] arr = new int[K+1];
 
-			for(int i=0; i<T; i++){				
-				int N = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
 
-				arr = new int[N+1];
-				dp = new int[N+1][N+1];
-				sum = new int[N+1][N+1];
-
-				StringTokenizer st = new StringTokenizer(br.readLine());
-
-				for(int j=1; j<N+1; j++){
-					arr[j] = Integer.parseInt(st.nextToken());
+			for(int i=1;i <K+1; i++){
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+	
+			for(int i=1; i<K+1; i++){
+				for(int j=i; j<K+1; j++){
+					sum[i][j] = sum[i][j-1] + arr[j];
 				}
-
-				for(int j=1; j<N+1; j++){
-					for(int k=j; k<N+1; k++){
-						sum[j][k] = sum[j][k-1] + arr[j];
+			}
+	
+			for(int i=2; i<K+1; i++){
+				for(int j=1; j<K+1-i+1; j++){
+					int k = j - 1 + i;
+					dp[j][k] = Integer.MAX_VALUE;
+					for(int l = j; l<k; l++){
+						dp[j][k] = Math.min(dp[j][k], dp[j][l] + dp[l+1][k] + sum[j][k]);
 					}
 				}
+			}
 
-				for(int j=1; j<N+1; j++){
-					for(int k=1; k<N+1; k++){
-						System.out.print(sum[j][k] + " ");
-					}
-				}
-				
-
-				for(int len = 2; len< N+1; len++){
-					for(int k=1; k<N+1 - len; k++){
-						int l = k + len - 1;
-						dp[k][l] = Integer.MAX_VALUE;
-
-						for(int m=k; m<l; m++){
-							dp[k][l] = Math.min(dp[k][l], dp[k][m] + dp[m+1][l] + sum[k][l]);
-						}
-					}
-				}
-
-				System.out.println(dp[1][N]);
-				
-			}			
-					
-			
+			// for(int a = 1; a<K+1; a++){
+			// 	for(int b = 1; b<K+1; b++){
+			// 		System.out.print(dp[a][b] + " ");
+			// 	}
+			// 	System.out.println();
+			// }
+	
+			System.out.println(dp[1][K]);
+		}
 	}
 }
