089b3882a6db316a958aaa2c32b102b16257deaa
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-07-16 17:31:19 +0900

    타노스

---
+++ baekjoon/15802/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, L, R;
+    static int[][] map;
+    static boolean[][] visit;
+    
+    static int[] dr = {-1,1,0,0};
+    static int[] dc = {0,0,-1,1};
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        System.out.println(1);
+    }
+}
