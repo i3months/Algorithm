647479039dcc149b72919968c25344910a041087
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-07-26 04:59:30 +0900

    가장 긴 증가하는 부분 수열

---
+++ baekjoon/11053/Main.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N+1];
+        int[] dp = new int[N+1];
+
+        StringTokenizer st=  new StringTokenizer(br.readLine());
+
+        for(int i=1; i<N+1; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.fill(dp, 1);
+        dp[1] = 1;
+
+        
+        int max = 1;
+
+        for(int i=2; i<N+1; i++){
+            for(int j=1; j<i; j++){
+                if(arr[j] < arr[i]){
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                    max = Math.max(max, dp[i]);
+                }
+            }
+        }
+
+        System.out.println(max);
+        
+
+        
+
+        
+        
+
+    }
+}    
