1b2b2885ad7f482f21511e8cd70ea1f70a9df6d2
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-07-26 19:06:09 +0900

    상근이의 여행

---
+++ baekjoon/9372/Main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<T; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            for(int j=0; j<M; j++){
+                st=new StringTokenizer(br.readLine());
+            }
+
+            System.out.println(N-1);
+        }
+
+    }
+}    
