19dfdc831828e8e1068c525b05ea41c5d40b1dc2
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-08-13 23:44:17 +0900

    저울

---
+++ baekjoon/2437/Main.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main { 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[N];
+
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int sum = 0;
+
+        for(int i=0; i<N; i++){
+            if(sum + 1 < arr[i]){
+                System.out.println(sum + 1);
+                return;
+            }
+
+            sum += arr[i];
+        }
+
+        System.out.println(sum + 1);
+        
+    }
+}
