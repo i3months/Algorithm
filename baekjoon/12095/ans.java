d75d1bbe8660b425e71aa226964497f9efa37793
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-08-16 15:45:38 +0900

    가장 오래걸리는 스도쿠

---
+++ baekjoon/12095/ans.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class ans {
+  public static void main(String[] args){
+    System.out.println("8 0 0 0 0 0 0 0 0\n0 0 3 6 0 0 0 0 0\n0 7 0 0 9 0 2 0 0\n0 5 0 0 0 7 0 0 0\n0 0 0 0 4 0 7 0 0\n0 0 0 1 0 0 0 3 0\n0 0 1 0 0 0 0 0 8\n0 0 8 5 0 0 0 1 0\n0 9 0 0 0 0 4 0 0");
+  }
+  
+	
+}
