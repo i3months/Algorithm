d75517f8d2630886ad3bc298f3cf41dafa67bc19
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-11-20 11:19:35 +0900

    키 순서

---
+++ baekjoon/2458/Main.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+
+    static final int INF = 987654321;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));                
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] adj = new int[N + 1][N + 1];
+
+        for(int i=0; i<N+1; i++) {
+            for(int j=0; j<N+1; j++) {
+                adj[i][j] = INF; 
+            }
+        }
+
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            adj[a][b] = 1;
+        }
+
+        // for(int i=1; i<N+1; i++) adj[i][i] = 0;
+
+        for(int k=1; k<N+1; k++) {
+            for(int i=1; i<N+1; i++) {
+                for(int j=1; j<N+1; j++) {
+                    adj[i][j] = Math.min(adj[i][j], adj[i][k] + adj[k][j]);
+                }
+            }
+        }
+
+
+        int ans = 0;
+
+        for(int i=1; i<N+1; i++) {
+            int cnt = 0;
+            for(int j=1; j<N+1; j++) {
+                if(adj[i][j] != INF || adj[j][i] != INF) {
+                    cnt++;                    
+                }
+            }
+
+            if(cnt == N - 1) {
+                // System.out.println(i);
+                ans++;
+            }
+        }
+
+        System.out.println(ans);
+        System.exit(0);
+
+
+        for(int i=1; i<N+1; i++) {
+            for(int j=1; j<N+1; j++) {
+
+                if(adj[i][j] == INF) System.out.print("#" + " "); else
+                System.out.print(adj[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        
+        
+        
+    }
+}
