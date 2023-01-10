0d9cc3847d70936c9bad806dd971f5a278575351
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2023-01-10 11:58:46 +0900

    Greatest Convex

---
+++ CodeForces/Codeforces Round #842 (Div. 2)/A/Main.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); 
+	cin.tie(0);
+
+	int TC;
+	cin >> TC;
+
+	while(TC-- > 0) {
+		int K;
+
+		cin >> K;
+
+		cout << K-1 << endl;
+	}
+}
